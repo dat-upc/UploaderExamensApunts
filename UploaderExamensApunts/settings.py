4218d71603db2849930c316cf2149cd453fee769
Aniol Martí <aniol@aniolmarti.cat>
2019-03-13 19:51:10 +0100

    Use MySQL insteaf of SQLite

--- UploaderExamensApunts/settings.py
+++ UploaderExamensApunts/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'UploaderExamensApunts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': DATABASE_MYSQL,
+        'USER': USER_NAME,
+        'PASSWORD': PASSWORD,
     }
 }
 
