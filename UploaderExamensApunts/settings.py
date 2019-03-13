17bfc28332dbecfc3357a155abe395ea7f88ea5c
Aniol Martí <aniol@aniolmarti.cat>
2019-03-13 19:20:21 +0100

    Add first view and remove secret settings from settings.py

--- UploaderExamensApunts/settings.py
+++ UploaderExamensApunts/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+try:
+    from .settings_secret import *
+except:
+    print ("Missing settings_secret.py file.")
+    quit()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$tv$ozu$k*$1-^q-&un(cbygn$8_00y=u=6w78bkhts_dw7lc7'
+SECRET_KEY = SECRET_KEY_SAVED
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
