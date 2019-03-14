f00d323afb04265326083799657c6a8827f60ba6
Aniol Martí <aniol@aniolmarti.cat>
2019-03-14 20:31:02 +0100

    Change __init__.py variable names

--- UploaderExamensApunts/__init__.py
+++ UploaderExamensApunts/__init__.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2019 Aniol Marti
 
 __author__        = "Aniol Marti"
-__author_email__  = "aniol.marti@upc.edu"
-__license_short__ = "GPLv3+"
-__license_long__  = "Licensed under the GNU GPL v3 or any later version"
+__email__  = "aniol.marti@upc.edu"
 __copyright__ = "Copyright 2019 {__author__}".format(**locals())
+__license__ = "GPLv3+"
