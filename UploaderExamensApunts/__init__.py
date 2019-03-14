1c7888df341bf18e4be203f9da74a25a57893cfd
Aniol Martí <aniol@aniolmarti.cat>
2019-03-14 20:26:46 +0100

    Add license and copyright information

---
+++ UploaderExamensApunts/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2019 Aniol Marti
+
+__author__        = "Aniol Marti"
+__author_email__  = "aniol.marti@upc.edu"
+__license_short__ = "GPLv3+"
+__license_long__  = "Licensed under the GNU GPL v3 or any later version"
+__copyright__ = "Copyright 2019 {__author__}".format(**locals())
