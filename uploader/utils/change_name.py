082fd4c6b1d1da9d2efd3d752f716b782f3bf5e9
Aniol Marti <aniol@aniolmarti.cat>
2019-03-22 12:14:04 +0100

    Exams follow a different format

--- uploader/utils/change_name.py
+++ uploader/utils/change_name.py
@@ -4,7 +4,16 @@ from UploaderExamensApunts.constants import *
 def change_name(instance, filename):
     extension = os.path.splitext(filename)[1]
     data = str(instance.upload_date.strftime("%d_%m"))
-    name = instance.assignatura + "-y." + instance.curs + "-" + instance.document + "-q." + \
-           instance.quadrimestre + "-d." + data + "-p." + instance.professor.replace(" ", "_") + "-a." + \
-           instance.alumne.replace(" ", "_") + extension
+    if (instance.document == "examen"):
+        name = instance.assignatura + "-y." + instance.curs + "-" + instance.document + "_" + \
+               instance.parcial_final + "_" + instance.tipus_examen + "-q." + instance.quadrimestre\
+               + "-d." + data + "-p." + instance.professor.replace(" ", "_") + "-a." + \
+               instance.alumne.replace(" ", "_")
+        if (instance.solucio):
+            name = name + "-s"
+        name = name + extension
+    else:
+        name = instance.assignatura + "-y." + instance.curs + "-" + instance.document + "-q." + \
+               instance.quadrimestre + "-d." + data + "-p." + instance.professor.replace(" ", "_") + "-a." + \
+               instance.alumne.replace(" ", "_") + extension
     return os.path.join(REL_TMP_DIR, name)
