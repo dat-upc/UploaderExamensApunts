2be6d1f3156d026578ade4c5f3641beee22695f4
Aniol Martí <aniol@aniolmarti.cat>
2019-03-24 20:08:14 +0100

    Add a boolean variable to be checked by an admin

--- uploader/models.py
+++ uploader/models.py
@@ -48,3 +48,4 @@ class Upload(models.Model):
     tipus_examen = models.CharField(max_length=MAX_LENGTH, choices=EMPTY+EXAM_TYPES, blank=True, null=True)
     solucio = models.BooleanField(default=False)
     file_upload = ContentTypeRestrictedFileField(upload_to=change_name, content_types=CONTENT_TYPES, max_upload_size=MAX_FILE_SIZE, null=True, blank=True)
+    is_correct = models.BooleanField(default=False)
