160854b842bebad9ae5cab2f91f872a45525f487
Aniol Martí <aniol@aniolmarti.cat>
2019-04-16 19:13:50 +0200

    Add degree information in the admin panel

--- uploader/admin.py
+++ uploader/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from uploader.models import Upload
 
 class UploadAdmin(admin.ModelAdmin):
-    list_display = ('upload_date', 'file_upload', 'is_correct')
+    list_display = ('upload_date', 'grau', 'file_upload', 'is_correct')
     exclude = ()
 
 admin.site.register(Upload, UploadAdmin)
