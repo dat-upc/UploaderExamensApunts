25bbb16285a018475fc50899896c8c019ca00b6e
Aniol Marti <aniol@aniolmarti.cat>
2019-10-24 18:56:41 +0200

    Add Person model in Django admin site

--- uploader/admin.py
+++ uploader/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from uploader.models import Upload
+from uploader.models import Upload, Person
 
 class UploadAdmin(admin.ModelAdmin):
     list_display = ('upload_date', 'grau', 'file_upload', 'is_correct')
     exclude = ()
 
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'dni', 'email')
+    exclude = ()
+
 admin.site.register(Upload, UploadAdmin)
+admin.site.register(Person, PersonAdmin)
